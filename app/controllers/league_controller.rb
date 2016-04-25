64f716c797e64237b3322ab46257f30ad0655d50
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-04-25 21:39:17 +0100

    unfinished

--- app/controllers/league_controller.rb
+++ app/controllers/league_controller.rb
@@ -2,5 +2,16 @@ class LeagueController < ApplicationController
 
   def index
     @client = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.3/game/by-summoner/85098161/recent?api_key=#{ENV['LOL_KEY']}")
+    @json = JSON.parse(@client)
+    @json.select {|win| @json["win"]}
+  end
+  #
+  def win
+    counter = 0
+    @json.each do
+      if @json["win"] = true
+        counter += 1
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :league
-  root to: "league#index.html.erb"
+  root to: "league#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
