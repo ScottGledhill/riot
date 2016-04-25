2edf2032e202e6efe62f9e1be2f6be4eaa529d71
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-04-25 18:28:35 +0100

    recent games call

--- app/controllers/league_controller.rb
+++ app/controllers/league_controller.rb
@@ -1,6 +1,6 @@
 class LeagueController < ApplicationController
 
-  def create
-    @client = RestClient.get "https://euw.api.pvp.net/api/lol/euw/v1.2/champion?api_key=#{ENV['LOL_KEY']}"
+  def index
+    @client = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.3/game/by-summoner/85098161/recent?api_key=#{ENV['LOL_KEY']}")
   end
 end
