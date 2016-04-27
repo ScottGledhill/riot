89db52d38b10bc1b758fa534f18c8cff9c72cf7c
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-04-27 12:41:36 +0100

    Summoner wins displayed

--- app/controllers/league_controller.rb
+++ app/controllers/league_controller.rb
@@ -4,13 +4,4 @@ class LeagueController < ApplicationController
     @client = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/81323101/summary?season=SEASON2016&api_key=#{ENV['LOL_KEY']}")
     @object = JSON.parse(@client)["playerStatSummaries"][2]["wins"]
   end
-  #
-  def win
-    counter = 0
-    @json.each do
-      if @json["win"] = true
-        counter += 1
-      end
-    end
-  end
 end
