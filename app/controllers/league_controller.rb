831b056cf11b1fa85b85275670610165d198dff4
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-05-03 18:02:19 +0100

    wins/losses found from params

--- app/controllers/league_controller.rb
+++ app/controllers/league_controller.rb
@@ -5,6 +5,8 @@ class LeagueController < ApplicationController
     @params_adjusted = @params.to_sym
     @name = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/#{@params_adjusted}?api_key=#{ENV['LOL_KEY']}")
     @summoner = JSON.parse(@name)[@params]["id"]
+    call
+
 
     @gang = CrowGang.new
     @gang.call
@@ -21,6 +23,12 @@ class LeagueController < ApplicationController
   end
 
   def call
-    # @client = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/82746988/summary?season=SEASON2016&api_key=#{ENV['LOL_KEY']}")
+     @client = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/#{@summoner}/summary?season=SEASON2016&api_key=#{ENV['LOL_KEY']}")
+     @loss = JSON.parse(@client)["playerStatSummaries"][2]["losses"]
+     @win = JSON.parse(@client)["playerStatSummaries"][2]["wins"]
   end
 end
+
+
+#call should grab whichever is searched and make an api call with it, now just
+#display it on the page along side the others
