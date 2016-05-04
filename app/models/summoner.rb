5e6ef3cc866eb9f31499f986fc3cf10dae625334
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-05-04 17:37:53 +0100

    model refactored, linked methods

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -1,10 +1,9 @@
 class Summoner < ActiveRecord::Base
 
   def retrieve_summoner_name_from_id(params)
-    @params = params[:q].downcase
-    @params_adjusted = @params.to_sym
-    @name = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/#{@params_adjusted}?api_key=#{ENV['LOL_KEY']}")
-    @summoner = JSON.parse(@name)[@params]["id"]
+    @params = params[:q].downcase.to_sym
+    @name = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/#{@params}?api_key=#{ENV['LOL_KEY']}")
+    @summoner = JSON.parse(@name)[@params.to_s]["id"]
     summoner_call
   end
 
