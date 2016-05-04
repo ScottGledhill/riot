cd381ac5a79f25bf59ff2aa75bcc0e469f573412
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-05-04 16:34:34 +0100

    view showing searched summoner

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -4,15 +4,24 @@ class Summoner < ActiveRecord::Base
     @params = params[:q]
     @params_adjusted = @params.to_sym
     @name = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/#{@params_adjusted}?api_key=#{ENV['LOL_KEY']}")
-    @summoner = JSON.parse(@name)[@params]["id"]
+    summoner_name
     summoner_call
   end
 
+  def summoner_name
+    @summoner = JSON.parse(@name)[@params]["id"]
+  end
+  
   def summoner_call
     @client = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/#{@summoner}/summary?season=SEASON2016&api_key=#{ENV['LOL_KEY']}")
-    p @loss = JSON.parse(@client)["playerStatSummaries"]
     # p  @loss.fetch("playerStatSummaryType").fetch("RankedSolo5x5").fetch("wins")
-    # [2]["losses"]
-    # @win = JSON.parse(@client)["playerStatSummaries"][2]["wins"]
+  end
+
+  def wins
+    @wins = JSON.parse(@client)["playerStatSummaries"][2]["wins"]
+  end
+
+  def losses
+    @losses = JSON.parse(@client)["playerStatSummaries"][2]["losses"]
   end
 end
