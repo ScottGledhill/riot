bc33afab14d5251bf84d91e5dcd2307067c64aae
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-05-04 12:55:27 +0100

    refactor

--- app/controllers/league_controller.rb
+++ app/controllers/league_controller.rb
@@ -1,8 +1,10 @@
 class LeagueController < ApplicationController
 
   def index
+    p params
     if !params.nil?
-      retrieve_summoner_name
+      @summon = Summoner.new
+      @summon.retrieve_summoner_name(params)
       @gang = CrowGang.new
       @gang.use_model
       @seven = Seven.new
@@ -11,21 +13,4 @@ class LeagueController < ApplicationController
       redirect_to('league#index')
     end
   end
-
-  def retrieve_summoner_name
-    @params = params[:q]
-    @params_adjusted = @params.to_sym
-    @name = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/#{@params_adjusted}?api_key=#{ENV['LOL_KEY']}")
-    p @summoner = JSON.parse(@name)[@params]["id"]
-  end
-
-  def summoner_call
-    @client = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/#{@summoner}/summary?season=SEASON2016&api_key=#{ENV['LOL_KEY']}")
-    @loss = JSON.parse(@client)["playerStatSummaries"][2]["losses"]
-    @win = JSON.parse(@client)["playerStatSummaries"][2]["wins"]
-  end
 end
-
-
-#call should grab whichever is searched and make an api call with it, now just
-#display it on the page along side the others
--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -1,10 +1,15 @@
 class Summoner < ActiveRecord::Base
 
-  def retrieve_summoner_name
-
+  def retrieve_summoner_name(params)
+    @params = params[:q]
+    @params_adjusted = @params.to_sym
+    @name = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/#{@params_adjusted}?api_key=#{ENV['LOL_KEY']}")
+    p @summoner = JSON.parse(@name)[@params]["id"]
   end
 
   def summoner_call
-    
+    @client = RestClient.get("https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/#{@summoner}/summary?season=SEASON2016&api_key=#{ENV['LOL_KEY']}")
+    @loss = JSON.parse(@client)["playerStatSummaries"][2]["losses"]
+    @win = JSON.parse(@client)["playerStatSummaries"][2]["wins"]
   end
 end
